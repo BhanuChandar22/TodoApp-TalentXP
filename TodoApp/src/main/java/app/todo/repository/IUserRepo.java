3d4cf4613e62dbba7cd632dee3aadd7d76931a0e
Bhanu Chandar <137188404+BhanuChandar22@users.noreply.github.com>
2025-06-02 00:28:05 +0530

    Delete TodoApp directory

--- TodoApp/src/main/java/app/todo/repository/IUserRepo.java
+++
@@ -1,9 +0,0 @@
-package app.todo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import app.todo.entity.UserEntity;
-
-public interface IUserRepo extends CrudRepository<UserEntity, Long> {
-
-}
--- TodoApp/src/main/java/app/todo/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package app.todo.service;
-
-
-import app.todo.pojo.UserDto;
-
-public interface IUserService {
-	
-	public UserDto createUser(UserDto userDto);
-}
